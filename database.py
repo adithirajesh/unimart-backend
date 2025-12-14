ae90717b3c3200e98f0c70d2df03f7823dba7706
adithirajesh <89330027+adithirajesh@users.noreply.github.com>
2025-12-14 18:33:06 +0000

    all changes

--- database.py
+++ database.py
@@ -4,15 +4,14 @@ import os
 db = SQLAlchemy()
 
 def init_db(app):
-    DATABASE_URL = os.environ.get("DATABASE_URL") or "sqlite:///unimarket.db"
-
-# For Render Postgres, SQLAlchemy expects psycopg:// -> postgresql://
+    from models import User, Product, UserActivity  # import models here
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    if os.environ.get("FLASK_ENV") != "production":
-        with app.app_context():
-            db.create_all()
+
+    with app.app_context():
+        db.create_all()  # <- This creates tables in Postgres
